603c57f4ca5b6bb3e4ed3bbb8f7cb542d9539f43
Chaim-shmaria <chaim.ddd@gmail.com>
2023-11-18 19:26:06 +0200

    first edit

--- משחק/executor.py
+++ משחק/executor.py
@@ -2,4 +2,20 @@ import pygame
 
 
 class Executor:
-    pass
+    def initialize_game_interface(self):
+        pass
+
+    def initialixe_variables(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def game_maintenance(self):
+        pass
+
+    def camera_movement(self):
+        pass
+
+    def quit(self):
+        pass
--- משחק/run_game.py
+++ משחק/run_game.py
@@ -1,3 +1,15 @@
 from executor import Executor
 
 executor = Executor()
+
+executor.initialize_game_interface()
+
+executor.initialixe_variables()
+
+executor.run_game()
+
+executor.game_maintenance()
+
+executor.camera_movement()
+
+executor.quit()
