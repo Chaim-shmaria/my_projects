3080aa053d66e3b061b88a30231a82bd069d489d
Chaim-shmaria <chaim.ddd@gmail.com>
2024-11-02 18:42:19 +0200

    refactor a mistake

--- bina-rooms/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const counterRoutes = require('./tempRoutes');
-
-// Use the counter routes
-router.use('/counter', counterRoutes);
-
-module.exports = router;
